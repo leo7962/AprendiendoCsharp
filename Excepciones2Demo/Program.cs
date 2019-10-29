764adcd0aeabb4bfe7cd2944de1aac8d6764960e
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-10-29 11:27:55 -0500

    Terminar modulo de excepciones

---
+++ Excepciones2Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Excepciones2Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int entero = int.MaxValue;
+
+            ////checked
+            ////{
+            ////     entero++;
+            ////}
+
+            //entero = unchecked(entero++);
+
+            //Console.WriteLine();
+
+            int mes = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(obtenerSignoZodiacal(mes));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Bloque Finally");
+            }
+
+
+            Console.ReadLine();
+        }
+        static string obtenerSignoZodiacal(int numeroMes)
+        {
+            string resultado = string.Empty;
+            switch (numeroMes)
+            {
+                case 1: resultado = "Aries"; break;
+                case 2: resultado = "Tauro"; break;
+                case 3: resultado = "Géminis"; break;
+                case 4: resultado = "Cáncer"; break;
+                case 5: resultado = "Leo"; break;
+                case 6: resultado = "Aries"; break;
+                case 7: resultado = "Virgo"; break;
+                case 8: resultado = "Escorpio"; break;
+                case 9: resultado = "Sagitario"; break;
+                case 10: resultado = "Capricornio"; break;
+                case 11: resultado = "Acuario"; break;
+                case 12: resultado = "Picis"; break;
+                default:
+                    throw new Exception("Mes invalido");
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
--- ExcepcionesDemo/Program.cs
+++ ExcepcionesDemo/Program.cs
@@ -6,15 +6,72 @@ namespace ExcepcionesDemo
     {
         static void Main(string[] args)
         {
-            //int valor= 10;
-            //Console.WriteLine(valor / 0);
-            //string numeroencadena = "a";
+            //Este es el manejo de excepciones 
 
-            //int valor = int.Parse(numeroencadena);
-            //string numerogrande = "99999999999";
-            //int numeroconvertido = int.Parse(numerogrande);
+           
 
             //Console.WriteLine(numeroconvertido);
+
+            //try
+            //{
+            //operaciones que queramos llevar
+            //}
+            //catch (tipo de excepción ex)
+            //{
+            //dado casi que ocurra una excepción
+            //}
+
+            try
+            {
+                int valor = 10;
+                Console.WriteLine(valor / 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("No se puede dividir entre cero" + ex);
+            }
+
+            try
+            {
+                //Convertir un string número a una cadena 
+                string numeroencadena = "1a";
+                int valor = int.Parse(numeroencadena);
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine("Unicamente se aceptan números" + ""+ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Solamente se aceptan números "+""+ex);
+            }
+
+            try
+            {
+                //string numerogrande = "99999999999";
+                //int numeroconvertido = int.Parse(numerogrande);
+                 string numeroencadena = "1a";
+                int valor = int.Parse(numeroencadena);
+            }
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (OverflowException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
 
             Console.ReadLine();
         }
