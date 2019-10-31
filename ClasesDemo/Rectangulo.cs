fd7619a6817376f9904b281241062e9601b243d6
Leonardo Fabián Hernández Peña <leonardohernandez.fp@academia.umb.edu.co>
2019-10-30 20:26:14 -0500

    Entrando a la introducción de Clases

---
+++ ClasesDemo/Rectangulo.cs
@@ -0,0 +1,24 @@
+﻿namespace ClasesDemo
+{
+    //Class nombre de clase
+    //{
+    //}
+    class Rectangulo
+    {
+        double altura;
+        double baseRectangulo;
+        string color;
+
+        public double CalcularArea()
+        {
+            // A = H * B            
+            return altura * baseRectangulo;
+        }
+
+        public double calcularPerimetro()
+        {
+            // P = 2 * H + 2 * b
+            return (2* altura) + (2*baseRectangulo);
+        }
+    }
+}
