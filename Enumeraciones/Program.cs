0c507eff02a7d8ea0e9c7a14f8c55af7f4ededab
leo7962 <ingenieroleonardo@outlook.com>
2019-11-25 22:28:14 -0500

    Entrando a enumeraciones

---
+++ Enumeraciones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enumeraciones
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var tipoUsuario = Rol.Administrador;
+            string temporal = tipoUsuario.ToString();
+
+            switch (tipoUsuario)
+            {
+                case Rol.Administrador:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+                case Rol.Desarrollador:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+                case Rol.Usuariofinal:
+                    Console.WriteLine(tipoUsuario);
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void ValidarUsuario(Rol rol)
+        {
+        }
+    }
+}
---
+++ Enumeraciones/Rol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumeraciones
+{
+    enum Rol
+    {
+        Administrador,
+        Desarrollador,
+        Usuariofinal
+    }
+}
