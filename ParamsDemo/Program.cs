d9a1fcfbbe6c250a43a327275da26c0617e73a64
Leonardo Fabián Hernández Peña <IngenieroLeonardo@outlook.com>
2020-01-15 21:05:38 -0500

    Se realiza aprendizaje arreglos de tipo parametros que realizan dinamismo a operaciones de arreglos de una sola dimensión.

---
+++ ParamsDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ParamsDemo
+{
+    class Program
+    {
+        //no se puede usar en arreglos multidimensionales, ni se puede sobrecargar sus metodos u operadores 
+        //debe ser el último parametro en una lista de parametros formales
+        //tiene prioridad sobre otras funciones
+        static void Main(string[] args)
+        {
+            int suma = Sumar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10000, "1.23");
+
+            Console.WriteLine(suma);
+        }
+
+        static int Sumar(params object[] numeros)
+        {
+            //if (numeros.Length < 2 || numeros == null)
+            //{
+            //    throw new ArgumentException("Debe agregar dos parametros como minimo");
+            //}
+            //else
+            //{
+            //    int suma = 0;
+            //    foreach (var numero in numeros)
+            //    {
+            //        suma += numero;
+            //    }
+
+            //    return suma;
+            //}
+
+            int suma = 0;
+            foreach (var numero in numeros)
+            {
+                if (numero is int)
+                {
+                    suma += (int)numero;
+                }
+                else if (numero is string)
+                {
+                    bool convertido =
+                        int.TryParse((string)numero, out int temporal);
+                    if (convertido)
+                    {
+                        suma += temporal;
+                    }
+                    else
+                    {
+                        throw new Exception("valor no numérico");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Valor no númerico");
+                }
+            }
+
+            return suma;
+        }
+    }
+}
